a72493eb80fa8aee48583a6b448892cd3c9035fb
FinnVAL <115410106+FinnVAL@users.noreply.github.com>
2024-02-24 17:46:21 +0700

    Create Lab04_b_Q2_1.py

---
+++ Lab04_b/Lab04_b_Q2/Lab04_b_Q2_1.py
@@ -0,0 +1,8 @@
+def count_words(file_name):
+    with open(file_name, 'r') as f:
+        t = f.read()
+    words = t.split()
+    num_words = len(words)
+    print('Total words are',num_words)
+
+count_words('story.txt')
